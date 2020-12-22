f3757f105dcb8970f307c7d3378b3823dbd8a616
Avin Sharma <sharmaavin@hotmail.com>
2020-12-22 16:58:33 -0500

    Update fragments to replicate functionality from respective activities

--- app/src/main/java/com/whomentors/aqua/screens/UserInfo/UserInfoFragment.kt
+++ app/src/main/java/com/whomentors/aqua/screens/UserInfo/UserInfoFragment.kt
@@ -1,11 +1,32 @@
 package com.whomentors.aqua.screens.UserInfo
 
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.whomentors.aqua.Activity.Start
+import com.whomentors.aqua.AppUtils.Thisapp
 import com.whomentors.aqua.R
+import kotlinx.android.synthetic.main.activity_init_user_info.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.util.*
 
 
 /**
@@ -15,23 +36,198 @@ import com.whomentors.aqua.R
  */
 class UserInfoFragment : Fragment() {
 
+    private var weight: String = ""
+    private var workTime: String = ""
+    private var name: String = ""
+    private var wakeupTime: Long = 0
+    private var sleepingTime: Long = 0
+    private lateinit var sharedPref: SharedPreferences
+    private var doubleBackToExitPressedOnce = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false)
+        val layoutView = inflater.inflate(R.layout.fragment_user_info, container, false)
+        val context = layoutView.context
+
+        val etWakeUpTime: TextInputLayout = layoutView.findViewById(R.id.etWakeUpTime)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+
+        // Find and initialize AdView
+        val mAdView: AdView = layoutView.findViewById(R.id.adView)
+        val adRequest =
+            AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        // Load interstitialAd
+        // https://developers.google.com/admob/android/interstitial
+        val mInterstitialAd = InterstitialAd(context)
+        mInterstitialAd.adUnitId = getString(R.string.g_inr)
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        mInterstitialAd.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                mInterstitialAd.show()
+            }
+
+            override fun onAdFailedToLoad(errorCode: Int) {
+                Log.d("", "hello")
+            }
+
+            override fun onAdOpened() {
+                Log.d("", "hello")
+            }
+
+            override fun onAdClicked() {
+                Log.d("", "hello")
+            }
+
+            override fun onAdLeftApplication() {
+                Log.d("", "hello")
+            }
+
+            override fun onAdClosed() {
+                Log.d("", "hello")
+            }
+        }
+
+
+
+        // Fetch wakeup and sleeping time from shared preferences
+
+        sharedPref = context.getSharedPreferences(Thisapp.USERS_SHARED_PREF, Thisapp.PRIVATE_MODE)
+
+        wakeupTime = sharedPref.getLong(Thisapp.WAKEUP_TIME, 1558323000000)
+        sleepingTime = sharedPref.getLong(Thisapp.SLEEPING_TIME_KEY, 1558369800000)
+
+        etWakeUpTime.editText!!.setOnClickListener {
+            // Create a calendar and set it's time to
+            // saved wakeup time
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = wakeupTime
+
+            // Create a timerDialog and set it's time to
+            // saved wakeup time
+            val mTimePicker: TimePickerDialog
+            mTimePicker = TimePickerDialog(
+                context,
+                TimePickerDialog.OnTimeSetListener { timePicker, selectedHour, selectedMinute ->
+
+                    val time = Calendar.getInstance()
+                    time.set(Calendar.HOUR_OF_DAY, selectedHour)
+                    time.set(Calendar.MINUTE, selectedMinute)
+
+                    // Assign selected time to the wakeupTime
+                    // variable to be saved later
+                    wakeupTime = time.timeInMillis
+
+                    etWakeUpTime.editText!!.setText(
+                        String.format("%02d:%02d", selectedHour, selectedMinute)
+                    )
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false
+            )
+            mTimePicker.setTitle("Select Wakeup Time")
+            mTimePicker.show()
+        }
+
+        // Display sleep time
+        etSleepTime.editText!!.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = sleepingTime
+
+            val mTimePicker: TimePickerDialog
+            mTimePicker = TimePickerDialog(
+                context,
+                TimePickerDialog.OnTimeSetListener { timePicker, selectedHour, selectedMinute ->
+
+                    val time = Calendar.getInstance()
+                    time.set(Calendar.HOUR_OF_DAY, selectedHour)
+                    time.set(Calendar.MINUTE, selectedMinute)
+
+                    // Assign selected time to the sleepTime
+                    // variable to be saved later
+                    sleepingTime = time.timeInMillis
+
+                    etSleepTime.editText!!.setText(
+                        String.format("%02d:%02d", selectedHour, selectedMinute)
+                    )
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false
+            )
+            mTimePicker.setTitle("Select Sleeping Time")
+            mTimePicker.show()
+        }
+
+        btnContinue.setOnClickListener {
+
+            val imm: InputMethodManager =
+                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(init_user_info_parent_layout.windowToken, 0)
+
+            weight = etWeight.editText!!.text.toString()
+            name = etName.editText!!.text.toString()
+            workTime = etWorkTime.editText!!.text.toString()
+
+            // Do empty checks
+            when {
+                TextUtils.isEmpty(name) -> Snackbar.make(
+                    layoutView,
+                    "Please Enter Your Name",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+
+                TextUtils.isEmpty(weight) -> Snackbar.make(
+                    layoutView,
+                    "Please input your weight",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                weight.toInt() > 200 || weight.toInt() < 20 -> Snackbar.make(
+                    layoutView,
+                    "Please input a valid weight",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+
+                TextUtils.isEmpty(workTime) -> Snackbar.make(
+                    layoutView,
+                    "Please input your workout time",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+
+                workTime.toInt() > 500 || workTime.toInt() < 0 -> Snackbar.make(
+                    layoutView,
+                    "Please input a valid workout time",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+
+
+                else -> {
+                    // If all the required fields are filled save
+                    // the values to shared preferences
+
+                    val editor = sharedPref.edit()
+                    editor.putInt(Thisapp.WEIGHT_KEY, weight.toInt())
+                    editor.putInt(Thisapp.WORK_TIME_KEY, workTime.toInt())
+                    editor.putLong(Thisapp.WAKEUP_TIME, wakeupTime)
+                    editor.putLong(Thisapp.SLEEPING_TIME_KEY, sleepingTime)
+                    editor.putBoolean(Thisapp.FIRST_RUN_KEY, false)
+
+                    val totalIntake = Thisapp.calculateIntake(weight.toInt(), workTime.toInt())
+                    val df = DecimalFormat("#")
+                    df.roundingMode = RoundingMode.CEILING
+                    editor.putInt(Thisapp.TOTAL_INTAKE, df.format(totalIntake).toInt())
+
+                    editor.apply()
+                    startActivity(Intent(this, Start::class.java))
+                    finish()
+
+                }
+            }
+        }
+        return layoutView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @return A new instance of fragment UserInfoFragment.
-         */
-
-        @JvmStatic
-        fun newInstance() = UserInfoFragment()
-    }
 }
--- app/src/main/java/com/whomentors/aqua/screens/WaterIntake/WaterIntakeFragment.kt
+++ app/src/main/java/com/whomentors/aqua/screens/WaterIntake/WaterIntakeFragment.kt
@@ -1,11 +1,32 @@
 package com.whomentors.aqua.screens.WaterIntake
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.whomentors.aqua.Activity.Start
+import com.whomentors.aqua.Activity.UserInfo
+import com.whomentors.aqua.AppUtils.Thisapp
+import com.whomentors.aqua.Helpers.Alarm
+import com.whomentors.aqua.Helpers.Sqlite
 import com.whomentors.aqua.R
+import kotlinx.android.synthetic.main.water_activity_main.*
 
 
 /**
@@ -15,12 +36,300 @@ import com.whomentors.aqua.R
  */
 class WaterIntakeFragment : Fragment() {
 
+    private var totalIntake: Int = 0
+    private var inTook: Int = 0
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var sqliteHelper: Sqlite
+    private lateinit var dateNow: String
+    private var notificStatus: Boolean = false
+    private var selectedOption: Int? = null
+    private var snackbar: Snackbar? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_water_intake, container, false)
+        val layoutView = inflater.inflate(R.layout.fragment_water_intake, container, false)
+        val context = layoutView.context
+
+        sharedPref = context.getSharedPreferences(Thisapp.USERS_SHARED_PREF, Thisapp.PRIVATE_MODE)
+        sqliteHelper = Sqlite(context)
+
+        val mAdView: AdView = layoutView.findViewById(R.id.adView)
+        val adRequest =
+            AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        // Load interstitialAd
+        // https://developers.google.com/admob/android/interstitial
+        val mInterstitialAd = InterstitialAd(context)
+        mInterstitialAd.adUnitId = getString(R.string.g_inr)
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        mInterstitialAd.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                mInterstitialAd.show()
+            }
+
+            override fun onAdFailedToLoad(errorCode: Int) {
+                Log.d("", "hello")
+            }
+
+            override fun onAdOpened() {
+                Log.d("", "hello")
+            }
+
+            override fun onAdClicked() {
+                Log.d("", "hello")
+            }
+
+            override fun onAdLeftApplication() {
+                Log.d("", "hello")
+            }
+
+            override fun onAdClosed() {
+                Log.d("", "hello")
+            }
+        }
+
+        // Goes back to Start Activity
+        btnBack.setOnClickListener {
+            startActivity(Intent(this, Start::class.java))
+            finish()
+        }
+
+        totalIntake = sharedPref.getInt(Thisapp.TOTAL_INTAKE, 0)
+
+        // Why should we go back to UserInfo if the intake is 0 and how would intake go below 0?
+        if (totalIntake <= 0) {
+            startActivity(Intent(this, UserInfo::class.java))
+            finish()
+        }
+
+        dateNow = Thisapp.getCurrentDate()!!
+
+        return layoutView
+    }
+
+    fun updateValues() {
+        totalIntake = sharedPref.getInt(Thisapp.TOTAL_INTAKE, 0)
+
+        inTook = sqliteHelper.getIntook(dateNow)
+
+        setWaterLevel(inTook, totalIntake)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val outValue = TypedValue()
+        context?.applicationContext?.theme?.resolveAttribute(
+            android.R.attr.selectableItemBackground,
+            outValue,
+            true
+        )
+
+        // Get notification status from shared preferences
+        notificStatus = sharedPref.getBoolean(Thisapp.NOTIFICATION_STATUS_KEY, true)
+        val alarm = Alarm()
+        if (!context?.let { alarm.checkAlarm(it) }!! && notificStatus) {
+            btnNotific.setImageDrawable(context!!.getDrawable(R.drawable.ic_bell))
+            alarm.setAlarm(
+                context!!,
+                sharedPref.getInt(Thisapp.NOTIFICATION_FREQUENCY_KEY, 30).toLong()
+            )
+        }
+
+        // Change notification icon based on notification status
+        if (notificStatus) {
+            btnNotific.setImageDrawable(context!!.getDrawable(R.drawable.ic_bell))
+        } else {
+            btnNotific.setImageDrawable(context!!.getDrawable(R.drawable.ic_bell_disabled))
+        }
+
+        sqliteHelper.addAll(dateNow, 0, totalIntake)
+
+        updateValues()
+
+        // Add the selected amount to total intake
+        // when + fab is pressed
+        fabAdd.setOnClickListener {
+
+
+            if (selectedOption != null) {
+                if ((inTook * 100 / totalIntake) <= 140) {
+
+                    if (sqliteHelper.addIntook(dateNow, selectedOption!!) > 0) {
+                        inTook += selectedOption!!
+                        setWaterLevel(inTook, totalIntake)
+
+                        Snackbar.make(it, "Your water intake was saved...!!", Snackbar.LENGTH_SHORT)
+                            .show()
+
+                    }
+                } else {
+                    Snackbar.make(it, "You already achieved the goal", Snackbar.LENGTH_SHORT).show()
+                }
+                selectedOption = null
+                t6.text = "Custom"
+                op50ml.background = context!!.getDrawable(outValue.resourceId)
+                op100ml.background = context!!.getDrawable(outValue.resourceId)
+                op150ml.background = context!!.getDrawable(outValue.resourceId)
+                op200ml.background = context!!.getDrawable(outValue.resourceId)
+                op250ml.background = context!!.getDrawable(outValue.resourceId)
+                opCustom.background = context!!.getDrawable(outValue.resourceId)
+            } else {
+                YoYo.with(Techniques.Shake)
+                    .duration(700)
+                    .playOn(carddd)
+                Snackbar.make(it, "Please select an option", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        btnNotific.setOnClickListener {
+            notificStatus = !notificStatus
+            sharedPref.edit().putBoolean(Thisapp.NOTIFICATION_STATUS_KEY, notificStatus).apply()
+            if (notificStatus) {
+                btnNotific.setImageDrawable(context!!.getDrawable(R.drawable.ic_bell))
+                Snackbar.make(it, "Notification Enabled..", Snackbar.LENGTH_SHORT).show()
+                alarm.setAlarm(
+                    context!!,
+                    sharedPref.getInt(Thisapp.NOTIFICATION_FREQUENCY_KEY, 30).toLong()
+                )
+            } else {
+                btnNotific.setImageDrawable(context!!.getDrawable(R.drawable.ic_bell_disabled))
+                Snackbar.make(it, "Notification Disabled..", Snackbar.LENGTH_SHORT).show()
+                alarm.cancelAlarm(context!!)
+            }
+        }
+
+
+
+        op50ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 50
+            op50ml.background = context!!.getDrawable(R.drawable.option_select_bg)
+            op100ml.background = context!!.getDrawable(outValue.resourceId)
+            op150ml.background = context!!.getDrawable(outValue.resourceId)
+            op200ml.background = context!!.getDrawable(outValue.resourceId)
+            op250ml.background = context!!.getDrawable(outValue.resourceId)
+            opCustom.background = context!!.getDrawable(outValue.resourceId)
+
+        }
+
+        op100ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 100
+            op50ml.background = context!!.getDrawable(outValue.resourceId)
+            op100ml.background = context!!.getDrawable(R.drawable.option_select_bg)
+            op150ml.background = context!!.getDrawable(outValue.resourceId)
+            op200ml.background = context!!.getDrawable(outValue.resourceId)
+            op250ml.background = context!!.getDrawable(outValue.resourceId)
+            opCustom.background = context!!.getDrawable(outValue.resourceId)
+
+        }
+
+        op150ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 150
+            op50ml.background = context!!.getDrawable(outValue.resourceId)
+            op100ml.background = context!!.getDrawable(outValue.resourceId)
+            op150ml.background = context!!.getDrawable(R.drawable.option_select_bg)
+            op200ml.background = context!!.getDrawable(outValue.resourceId)
+            op250ml.background = context!!.getDrawable(outValue.resourceId)
+            opCustom.background = context!!.getDrawable(outValue.resourceId)
+
+        }
+
+        op200ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 200
+            op50ml.background = context!!.getDrawable(outValue.resourceId)
+            op100ml.background = context!!.getDrawable(outValue.resourceId)
+            op150ml.background = context!!.getDrawable(outValue.resourceId)
+            op200ml.background = context!!.getDrawable(R.drawable.option_select_bg)
+            op250ml.background = context!!.getDrawable(outValue.resourceId)
+            opCustom.background = context!!.getDrawable(outValue.resourceId)
+
+        }
+
+        op250ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 250
+            op50ml.background = context!!.getDrawable(outValue.resourceId)
+            op100ml.background = context!!.getDrawable(outValue.resourceId)
+            op150ml.background = context!!.getDrawable(outValue.resourceId)
+            op200ml.background = context!!.getDrawable(outValue.resourceId)
+            op250ml.background = context!!.getDrawable(R.drawable.option_select_bg)
+            opCustom.background = context!!.getDrawable(outValue.resourceId)
+
+        }
+
+        opCustom.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+
+            val li = LayoutInflater.from(context)
+            val promptsView = li.inflate(R.layout.custom_input_dialog, null)
+
+            val alertDialogBuilder = AlertDialog.Builder(context!!)
+            alertDialogBuilder.setView(promptsView)
+
+            val userInput = promptsView
+                .findViewById(R.id.etCustomInput) as TextInputLayout
+
+            alertDialogBuilder.setPositiveButton("OK") { dialog, id ->
+                val inputText = userInput.editText!!.text.toString()
+                if (!TextUtils.isEmpty(inputText)) {
+                    t6.text = "${inputText} ml"
+                    selectedOption = inputText.toInt()
+                }
+            }.setNegativeButton("Cancel") { dialog, id ->
+                dialog.cancel()
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+
+            op50ml.background = context!!.getDrawable(outValue.resourceId)
+            op100ml.background = context!!.getDrawable(outValue.resourceId)
+            op150ml.background = context!!.getDrawable(outValue.resourceId)
+            op200ml.background = context!!.getDrawable(outValue.resourceId)
+            op250ml.background = context!!.getDrawable(outValue.resourceId)
+            opCustom.background = context!!.getDrawable(R.drawable.option_select_bg)
+
+        }
+
+    }
+
+
+    private fun setWaterLevel(inTook: Int, totalIntake: Int) {
+
+        YoYo.with(Techniques.SlideInDown)
+            .duration(500)
+            .playOn(tvIntook)
+        tvIntook.text = "$inTook"
+        tvTotalIntake.text = "$totalIntake ml"
+        val progress = ((inTook / totalIntake.toFloat()) * 100).toInt()
+        YoYo.with(Techniques.Pulse)
+            .duration(500)
+            .playOn(intakeProgress)
+        intakeProgress.currentProgress = progress
+        if ((inTook * 100 / totalIntake) > 140) {
+            Snackbar.make(main_activity_parent, "You achieved the goal", Snackbar.LENGTH_SHORT)
+                .show()
+        }
     }
 
 }
