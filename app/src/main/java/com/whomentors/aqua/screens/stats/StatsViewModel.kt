1b30df7ec80d3c131f8f04ca8849fad78eda759b
Avin Sharma <sharmaavin@hotmail.com>
2021-02-09 11:56:48 -0500

    Create StatsViewModel.kt

---
+++ app/src/main/java/com/whomentors/aqua/screens/stats/StatsViewModel.kt
@@ -0,0 +1,31 @@
+package com.whomentors.aqua.screens.stats
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.whomentors.aqua.database.DailyIntake
+import com.whomentors.aqua.database.StatsDatabaseDao
+import kotlinx.coroutines.launch
+
+class StatsViewModel(
+    val database: StatsDatabaseDao,
+    application: Application
+) : AndroidViewModel(application) {
+
+    private lateinit var allEntries: LiveData<List<DailyIntake>>
+    init {
+        initializeAllEntries()
+    }
+
+    private fun initializeAllEntries(){
+        viewModelScope.launch {
+            allEntries = database.getAllIntakes()
+        }
+    }
+
+    fun getAllEntries(): LiveData<List<DailyIntake>>{
+        return allEntries
+    }
+}
